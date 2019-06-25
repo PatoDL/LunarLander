e3156cc4691cdada4aea190cd392fb30eb231e4f
Pato <patodl2701@gmail.com>
2019-06-25 18:14:10 -0300

    improved ui animations

--- Lunar Lander/Assets/Assets/Scripts/UICanvas.cs
+++ Lunar Lander/Assets/Assets/Scripts/UICanvas.cs
@@ -9,19 +9,29 @@ public class UICanvas : MonoBehaviour
 
     public Button playButton;
 
-    public GameObject firstSplashScreen;
+    public GameObject fadePanel;
 
     public Button quitButton;
 
+    public GameObject title;
+
     float buttonYOffset = -1f;
+
+    Bounds bounds;
+
+    Vector3 titleTruePos;
 
     void Start()
     {
+        bounds = CameraUtils.OrthographicBounds();
         Vector3 pbPos = playButton.transform.position;
         uiRocket.transform.position = new Vector3(pbPos.x, pbPos.y + buttonYOffset);
-        firstSplashScreen.GetComponent<Image>().material.color = Color.white;
+        fadePanel.GetComponent<Image>().material.color = Color.white;
         FadeLogo();
         playButton.onClick.AddListener(FadeLogo);
+        titleTruePos = title.transform.position;
+        title.GetComponent<Image>().material.color = new Color(1, 1, 1, 0);
+        title.transform.position = bounds.max / 2;
     }
 
     void Update()
@@ -43,17 +53,55 @@ public class UICanvas : MonoBehaviour
 
     public void FadeLogo()
     {
-        StartCoroutine(Fade(firstSplashScreen));
+        GameObject logo = fadePanel.transform.Find("CachuflitoGamesLogo").gameObject;
+        logo.GetComponent<Image>().material.color = Color.white;
+        StartCoroutine(Fade(logo));
     }
 
     IEnumerator RocketStartAnimation()
     {
         float yScale = uiRocket.transform.localScale.y;
-        Bounds bounds = CameraUtils.OrthographicBounds();
-        bool rocketOutOfView = uiRocket.transform.position.x < bounds.max.y + yScale;
-        while (rocketOutOfView)
+        
+        bool rocketOutOfView = uiRocket.transform.position.y > bounds.max.y + yScale;
+        while (!rocketOutOfView)
         {
+            rocketOutOfView = uiRocket.transform.position.y > bounds.max.y + yScale;
             uiRocket.transform.position += Vector3.up * Time.deltaTime * 5f;
+            if (rocketOutOfView)
+            {
+                LevelManager.Get().GoToNextLevel();
+            }
+            yield return null;
+        }
+    }
+
+    public void GameStart()
+    {
+        StartCoroutine(RocketStartAnimation());
+    }
+
+    IEnumerator UnFade(GameObject g)
+    {
+        yield return new WaitForSeconds(2);
+        float t = 0;
+        Material mat = g.GetComponent<Image>().material;
+        Color c = mat.color;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            c.a = t;
+            mat.color = c;
+            if (t >= 1)
+            {
+                yield return new WaitForSeconds(1);
+                while(title.transform.position.y<titleTruePos.y)
+                {
+                    title.transform.position += Vector3.up * Time.deltaTime;
+                    yield return null; 
+                }
+                StartCoroutine(Fade(fadePanel));
+            }
             yield return null;
         }
     }
@@ -72,8 +120,16 @@ public class UICanvas : MonoBehaviour
             mat.color = c;
             if(t<=0)
             {
+                if (g.name == "CachuflitoGamesLogo")
+                {
+                    yield return new WaitForSeconds(1);
+                    StartCoroutine(UnFade(title));
+                }
+                else if(g.name=="FadePanel")
+                {
+                    uiRocket.GetComponent<ParticleSystem>().Play();
+                }
                 g.SetActive(false);
-                //StartCoroutine(RocketStartAnimation());
             }
             yield return null;
         }
